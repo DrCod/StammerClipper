d43c6dbfe19c54cbeb9df78ec1644a6a223aff90
Ronny <polleronny@yahoo.com>
2019-07-12 08:02:03 +0000

    Add model

--- data.py
+++ data.py
@@ -8,6 +8,17 @@ from python_speech_features import mfcc, logfbank
 import librosa
 
 
+
+def find_fft(signal, rate):
+    n = len(signal)
+    freq = np.fft.rfftfreq(n ,d =1/rate)
+    Y = np.abs(np.fft.rfft(signal)/n)
+    return (Y, freq)
+
+
+
+
+
 #plot signals
 
 def plot_signals(signals):
@@ -18,8 +29,6 @@ def plot_signals(signals):
         for y in range(5):
             axes[x,y].set_title(lists(signals).keys()[i]) 
             axes[x,y].plot(lists(signals).values()[i])
-            axes[x,y].get_xaxis().setVisible(False)
-            axes[x,y].get_yaxis().setVisible(False)
             i += 1
     
 # plot fourier transformed signals    
@@ -31,8 +40,6 @@ def plot_fft(fft):
         for y in range(5):
             axes[x,y].set_title(lists(fft).keys()[i]) 
             axes[x,y].plot(lists(fft).values()[i])
-            axes[x,y].get_xaxis().setVisible(False)
-            axes[x,y].get_yaxis().setVisible(False)
             i += 1
     
     
@@ -44,13 +51,48 @@ def plot_fbank(fbank):
         for y in range(5):
             axes[x,y].set_title(lists(fbank).keys()[i]) 
             axes[x,y].plot(lists(fbank).values()[i])
-            axes[x,y].get_xaxis().setVisible(False)
-            axes[x,y].get_yaxis().setVisible(False)
             i += 1
-            
-            
-         
 
+            
+def plot_mfcc(mfccs):
+    fig, axes =plt.subplots(nrows =2, ncols=5,sharex=False,sharey=True,figsize=(20,5))
+    fig.suptitle("Mfccs",size =16)
+    i =0
+    for x in range(2):
+        for y in range(5):
+            axes[x,y].set_title(lists(mfccs).keys()[i]) 
+            axes[x,y].plot(lists(mfccs).values()[i])
+            i += 1
+                
+            
+
+fft ={}
+fbank ={}
+signals ={}
+mfccs ={}
+
+for f in os.listdir('AllAudioWav'):
+    signal,rate =librosa.load('AllAudioWav/'+f,sr=2048)
+    signals[f] = signal
+    fft[f] =find_fft(signal,rate)
+    bank =logfbank(signal[:rate], rate, nfilt  =26, nfft=51).T
+    fbank[f] =bank
+    mel =mfcc(signal[:rate], rate, numcep =13, nfilt =26, nfft =51).T
+    mfccs[f] = mel
+    
+
+plot_signals(signals)
+plt.show()
+
+plot_fft(fft)
+plt.show()
+
+plot_fbank(fbank)
+plt.show()
+
+plot_mfcc(mfccs)
+plt.show()
+    
             
             
             
